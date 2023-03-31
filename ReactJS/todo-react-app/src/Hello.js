b53bf72a1b11269c16a6af508b722ea128c4b3b8
Vinca0121 <qqw7820@gmail.com>
2023-03-31 16:11:41 +0900

    re
    Signed-off-by: Vinca0121 <qqw7820@gmail.com>

--- ReactJS/todo-react-app/src/Hello.js
+++ ReactJS/todo-react-app/src/Hello.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-class Hello extends React.Component {
-    render(){
+// class Hello extends React.Component {
+//     render(){
+//             return(
+//                 <div className="Hello">
+//                 <label for="hello">Hello moto1</label>
+//                 </div>
+//             );
+//         }
+// }
+
+
+function Hello() {
             return(
                 <div className="Hello">
                 <label for="hello">Hello moto1</label>
                 </div>
-            );
-        }
+            );       
 }
 
 export default Hello;
