21ba6d8b8214e91afbd3f2c0767ef4068309f37f
Vinca0121 <qqw7820@gmail.com>
2023-03-31 09:19:26 +0900

    first

---
+++ project_2_ye/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const server = express();
+
+server.use(express.static(__dirname + "/public"));
+
+server.get("/", (req, res) => {
+ 
+  res.sendFile(__dirname + "/home.html");
+});
+
+server.listen(3000, (err) => {
+  if (err) return console.log(err);
+  console.log("The server is listening on port 3000");
+});
