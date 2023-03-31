f14192603ce2c72aa231cff3683169d5c03cc434
Vinca0121 <qqw7820@gmail.com>
2023-03-31 16:08:35 +0900

    re

---
+++ ReactJS/todo-react-app/src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    render(){
+            return(
+                <div className="Hello">
+                <label for="hello">Hello moto1</label>
+                </div>
+            );
+        }
+}
+
+export default Hello;
+
